440813dbe829d16c46a9da94ee842700fcc4d686
Marcílio de Oliveira Freitas <20211148060017@ifrn.local>
2023-04-19 15:37:27 -0300

    Atribuindo a um projeto.

---
+++ Corretor2.pas
@@ -0,0 +1,34 @@
+﻿unit Corretor2;
+
+interface
+
+type
+  // Criacaoo da classe
+  TCorretor = class
+  // Criando o bloco de funcoes como publico
+  nome: String;
+  data: String;
+  salario: Single;
+  lucroMensal: Integer;
+  public
+    function calcularComissao(lucroVenda: Single): Single;
+  end;
+
+implementation
+
+{ TCorretor }
+
+function TCorretor.calcularComissao(lucroVenda: Single): Single;
+begin
+  if True then
+  begin
+    Result := 1;
+  end
+  else
+  begin
+    Result := 1;
+  end;
+end;
+
+end.
+
